799f7366f1961f49c5d0465428a7250c33864cee
Shanna <s.vangrevengoed@arcady.nl>
2022-08-02 10:46:00 +0200

    leetcode studyplan programmingskills

---
+++ 1491AverageSalary/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//accepted, 128ms 45,54% faster than others
+double Average(int[] salary)
+{
+    double max = salary.Max();
+    double min = salary.Min();
+    double sum = 0;
+    for (int i = 0; i < salary.Length; i++)
+    {
+        if (salary[i] != min && salary[i] != max)
+        {
+            sum += salary[i];
+        }
+    }
+
+    return Math.Abs(sum / (salary.Length - 2));
+
+}
+int[] sal = { 48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000 };
+var x = Average(sal);
+Console.WriteLine(x);
+Console.WriteLine("Hello, World!");
---
+++ 1523CountOddNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//accepted by leetcode, 21 ms runtime 97,02% faster than other 
+int CountOdds(int low, int high)
+{
+    int count = (high - low) / 2;
+    if (low % 2 == 1 || high % 2 == 1)
+    {
+        count++;
+    }
+
+    return count;
+}
+
+var y = CountOdds(3, 7);
+var x = CountOdds(8, 1000000000);
+Console.WriteLine(x);
+
+Console.WriteLine("Hello, World!");
